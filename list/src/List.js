b17eaf1ab634fbb57f597b5a8b0c3739370e69ae
Himalaya <yugmani@yahoo.com>
2021-01-27 00:47:51 -0800

    add, edit, delete, alert:Added

---
+++ list/src/List.js
@@ -0,0 +1,35 @@
+import React from "react";
+// import { FaEdit, FaTrash } from "react-icons/fa";
+
+function List({ items, removeItem, editItem }) {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={() => editItem(id)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
+                Delete
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+}
+
+export default List;
